215ca49fab566261a2667f1a9f3311b82c81757e
Luis Feijoo <luis7fj@gmail.com>
2020-10-03 21:52:43 -0500

    Ajustes generales

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ window.Vue = require('vue');
 
 
 Vue.component('mesa-partes', require('./components/MesaPartes.vue').default);
+Vue.component('carousel', require('./components/Carousel').default);
 
 var vm = new Vue({
     el: '#app'
---
+++ resources/js/components/Carousel.vue
@@ -0,0 +1,42 @@
+<template>
+    <splide :options="options">
+        <splide-slide>
+            <img src="/img/carousel/1.jpg">
+        </splide-slide>
+        <splide-slide>
+            <img src="/img/carousel/2.jpg">
+        </splide-slide>
+        <splide-slide>
+            <img src="/img/carousel/3.jpg">
+        </splide-slide>
+    </splide>
+</template>
+
+<script>
+    import { Splide, SplideSlide } from '@splidejs/vue-splide';
+
+    export default {
+        components: {
+            Splide,
+            SplideSlide,
+        },
+        data () {
+            return {
+                options: {
+                    type:'loop',
+                    heightRatio: 0.25,
+                    cover: true,
+                    breakpoints: {
+                        640: {
+                            height: '17rem',
+                        }
+                    }
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
